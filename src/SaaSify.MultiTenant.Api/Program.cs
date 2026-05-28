a645d9c23929071124e1f88c83e347b596330a3a
Bibek Chaudhary <bibek4929@gmail.com>
2026-05-28 15:37:08 +0545

    Add database seeding and initial Identity migration

--- src/SaaSify.MultiTenant.Api/Program.cs
+++ src/SaaSify.MultiTenant.Api/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Identity;
 using SaaSify.MultiTenant.Api.Extensions;
 using SaaSify.MultiTenant.Infrastructure;
+using SaaSify.MultiTenant.Infrastructure.Identity.Entities;
+using SaaSify.MultiTenant.Infrastructure.Persistence.Contexts;
+using SaaSify.MultiTenant.Infrastructure.Persistence.Seed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +27,24 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context =
+        services.GetRequiredService<MasterDbContext>();
+
+    var roleManager =
+        services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+    var userManager =
+        services.GetRequiredService<UserManager<IdentityApplicationUser>>();
+
+    await ApplicationDbSeeder.SeedAsync(
+        context,
+        roleManager,
+        userManager);
+}
 
 app.Run();
---
+++ src/SaaSify.MultiTenant.Infrastructure/Persistence/Seed/ApplicationDbSeeder.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SaaSify.MultiTenant.Core.Constants;
+using SaaSify.MultiTenant.Infrastructure.Identity.Entities;
+using SaaSify.MultiTenant.Infrastructure.Persistence.Contexts;
+
+namespace SaaSify.MultiTenant.Infrastructure.Persistence.Seed
+{
+    public static class ApplicationDbSeeder
+    {
+        public static async Task SeedAsync(MasterDbContext context, RoleManager<IdentityRole<Guid>> roleManager, UserManager<IdentityApplicationUser> userManager)
+        {
+            await context.Database.MigrateAsync();
+
+            var roles = new[]
+            {
+                Roles.SuperAdmin,
+                Roles.Admin,
+                Roles.Employee
+            };
+
+            foreach (var role in roles)
+            {
+                var exists = await roleManager.RoleExistsAsync(role);
+
+                if (!exists)
+                {
+                    await roleManager.CreateAsync(new IdentityRole<Guid>(role));
+                }
+            }
+
+            var email = "assessment@yopmail.com";
+
+            var userExists = await userManager.FindByEmailAsync(email);
+
+            if(userExists is null)
+            {
+                var user = new IdentityApplicationUser {
+                    Id = Guid.NewGuid(),
+                    Email = email,
+                    UserName = email,
+                    NormalizedEmail = email.ToUpper(),
+                    NormalizedUserName = email.ToUpper(),
+                    EmailConfirmed = true,
+                    TenantId = null
+                };
+
+                var result = await userManager.CreateAsync(user, "Tester@123");
+
+                if(result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, Roles.SuperAdmin);
+                }
+            }
+        }
+    }
+}
