22d30f0f019495feaa0d31bd1b496d6d6160ddef
Bibek Chaudhary <bibek4929@gmail.com>
2026-05-28 14:23:05 +0545

    Add core domain entities and abstractions and shared API response wrapper and constants

---
+++ src/SaaSify.MultiTenant.Core/Entities/Employee .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaaSify.MultiTenant.Core.Entities
+{
+    internal class Employee
+    {
+    }
+}
