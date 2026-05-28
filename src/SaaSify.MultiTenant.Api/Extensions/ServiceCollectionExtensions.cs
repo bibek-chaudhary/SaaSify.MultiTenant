778727fd59361026ecb442758d0141fc351176c6
Bibek Chaudhary <bibek4929@gmail.com>
2026-05-28 14:08:58 +0545

    Configure API versioning and Swagger

---
+++ src/SaaSify.MultiTenant.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning;
+using Microsoft.OpenApi;
+
+namespace SaaSify.MultiTenant.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+
+            options.AssumeDefaultVersionWhenUnspecified = true;
+
+            options.ReportApiVersions = true;
+        })
+        .AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+
+            options.SubstituteApiVersionInUrl = true;
+        });
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "SaaSify MultiTenant API",
+                Version = "v1"
+            });
+        });
+
+        return services;
+    }
+}
