04023ffc2e24ef48ebbf1a25646bac438de62fa7
Bibek Chaudhary <bibek4929@gmail.com>
2026-05-30 16:19:44 +0545

    Add employee entity and tenant database schema

--- src/SaaSify.MultiTenant.Infrastructure/Persistence/Configurations/EmployeeConfiguration.cs
+++ src/SaaSify.MultiTenant.Infrastructure/Persistence/Configurations/EmployeeConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaaSify.MultiTenant.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,25 @@ using System.Threading.Tasks;
 
 namespace SaaSify.MultiTenant.Infrastructure.Persistence.Configurations
 {
-    internal class EmployeeConfiguration
+    public sealed class EmployeeConfiguration
+        : IEntityTypeConfiguration<Employee>
     {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employees");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FullName)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.EmailAddress)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.HasIndex(x => x.EmailAddress)
+                .IsUnique();
+        }
     }
 }
